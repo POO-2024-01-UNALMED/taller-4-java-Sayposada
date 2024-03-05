d2e86ecd6ce2f84b5829ec1ddb7e5c82435d57b2
Sayposada <siposadaa@unal.edu.co>
2024-03-05 11:38:01 -0500

    ingresamos las clases al paquete classroom

---
+++ .github/classroom/Asignatura.java
@@ -0,0 +1,50 @@
+package classroom;
+public class Asignatura {
+
+  public String nombre;
+  public String n = nombre;
+  public int codigoInterno;
+  public int codigoExterno;
+  public Tipo tipo;
+
+  public Asignatura() {
+    this("Sin nombre",Tipo.FUNDAMENTACION);
+  }
+  public Asignatura(int codigoExterno,Tipo tipo) {
+    this("Sin nombre", 0, codigoExterno,tipo);
+  }
+
+  public Asignatura(String nombre, Tipo tipo) {
+    this(nombre, 0, 0,tipo);
+    }
+
+  public Asignatura(String nombre, int codigoInterno, int codigoExterno,Tipo tipo) {
+    this.nombre = nombre;
+    this.codigoInterno = codigoInterno;
+    this.codigoExterno = codigoExterno;
+    this.tipo = tipo;
+    }
+
+  public void cambiarDatos(int codigoInterno, int codigoExterno, String nombre,Tipo tipo) {
+    this.codigoInterno = codigoInterno;
+    this.codigoExterno = codigoExterno;
+    this.nombre = nombre;
+    this.tipo = tipo;
+    }
+
+  public void cambiarDatos(int codigoExterno) {
+    this.codigoExterno = codigoExterno;
+    }
+
+  public void cambiarDatos(String nombre) {
+    this.nombre = nombre;
+    }
+
+  public void setCodigoInterno(int codigoInterno) {
+    this.codigoInterno = codigoInterno;
+    } 
+    
+   public void setCodigoInterno(double codigoInterno) {
+    this.codigoInterno = (int) codigoInterno;
+    }
+}
